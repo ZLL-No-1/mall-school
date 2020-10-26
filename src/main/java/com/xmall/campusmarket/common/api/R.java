f79d5a281786be61467a7ecff2ba17850538dac4
zhanglan <1258160027@qq.com>
2020-10-26 18:11:56 +0800

    校园二手交易市场0.1

--- src/main/java/com/xmall/campusmarket/common/api/R.java
+++ src/main/java/com/xmall/campusmarket/common/api/R.java
@@ -29,6 +29,77 @@ public class R<T> {
      * @param data 获取的数据
      */
     public static <T> R<T> success(T data){
-        return new R<T>();
+        return new R<T>(ResultCode.SUCCESS.getCode(),ResultCode.SUCCESS.getMessage(),data);
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data 获取的数据
+     * @param  message 提示信息
+     */
+    public static <T> R<T> success(T data, String message) {
+        return new R<T>(ResultCode.SUCCESS.getCode(), message, data);
+    }
+
+    /**
+     * 失败返回结果
+     * @param errorCode 错误码
+     */
+    public static <T> R<T> failed(IErrorCode errorCode) {
+        return new R<T>(errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param errorCode 错误码
+     * @param message 错误信息
+     */
+    public static <T> R<T> failed(IErrorCode errorCode,String message) {
+        return new R<T>(errorCode.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param message 提示信息
+     */
+    public static <T> R<T> failed(String message) {
+        return new R<T>(ResultCode.FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     */
+    public static <T> R<T> failed() {
+        return failed(ResultCode.FAILED);
+    }
+
+    /**
+     * 参数验证失败返回结果
+     */
+    public static <T> R<T> validateFailed() {
+        return failed(ResultCode.VALIDATE_FAILED);
+    }
+
+    /**
+     * 参数验证失败返回结果
+     * @param message 提示信息
+     */
+    public static <T> R<T> validateFailed(String message) {
+        return new R<T>(ResultCode.VALIDATE_FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 未登录返回结果
+     */
+    public static <T> R<T> unauthorized(T data) {
+        return new R<T>(ResultCode.UNAUTHORIZED.getCode(), ResultCode.UNAUTHORIZED.getMessage(), data);
+    }
+
+    /**
+     * 未授权返回结果
+     */
+    public static <T> R<T> forbidden(T data) {
+        return new R<T>(ResultCode.FORBIDDEN.getCode(), ResultCode.FORBIDDEN.getMessage(), data);
     }
 }
